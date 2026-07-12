2d42e9ea3f03beb45913fdc1386ed3de8cec3714
Neha-Maurya7068 <er.nehamaurya7068@gmail.com>
2026-07-12 14:49:14 +0530

    First API help of Spring Boot.

---
+++ src/main/java/com/javafullstack/neha/controller/FirstController.java
@@ -0,0 +1,24 @@
+package com.javafullstack.neha.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FirstController {
+	@GetMapping
+	public String hello() {
+		return "Hello Neha Mausi hello";
+	}
+	
+	@GetMapping("/hello")
+	public String world() {
+		return "Hello Neha Mausi world";
+	}
+	
+	@GetMapping("/neha")
+	public String neha() {
+		return "Hello Neha Mausi Neha Maurya";
+	}
+}
